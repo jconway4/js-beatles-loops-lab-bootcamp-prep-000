e0c55cc5c9b155619df58103532557c7aabed26c
jconway4 <jconwa4@illinois.edu>
2018-03-14 22:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ var theBeatlesPlay = function(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++) {
     finalArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  
   return finalArray;
 };
+
